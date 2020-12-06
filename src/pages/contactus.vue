bc647443605b20103a30568dc27c36fa15e0e09f
Rock <xiu43317@gmail.com>
2020-12-07 04:17:14 +0800

    set all function into vuex

--- src/pages/contactus.vue
+++ src/pages/contactus.vue
@@ -1,150 +1,99 @@
 <template>
   <div class="container mt-4">
-    <div class="input-group mb-3">
-      <div class="input-group-prepend">
-        <span class="input-group-text" id="basic-addon1">待辦事項</span>
-      </div>
-      <input
-        type="text"
-        class="form-control"
-        placeholder="準備要做的任務"
-        v-model="newTodo"
-      />
-      <div class="input-group-append">
-        <button class="btn btn-primary" type="button" @click="addTodo">
-          新增
-        </button>
-      </div>
+  <div class="input-group mb-3">
+    <div class="input-group-prepend">
+      <span class="input-group-text" id="basic-addon1">待辦事項</span>
     </div>
-    <div class="card text-center">
-      <div class="card-header">
-        <ul class="nav nav-tabs card-header-tabs">
-          <li class="nav-item">
-            <a class="nav-link" :class="{'active':visbility=='all'}" @click="visibility='all'" href="#/contactus">全部</a>
-          </li>
-          <li class="nav-item">
-            <a class="nav-link" :class="{'active': visibility == 'active'}" @click="visibility='active'" href="#/contactus">進行中</a>
-          </li>
-          <li class="nav-item" :class="{'active': visibility == 'completed'}" @click="visibility='completed'">
-            <a class="nav-link" href="#/contactus">已完成</a>
-          </li>
-        </ul>
-      </div>
-      <ul class="list-group list-group-flush text-left">
-        <li class="list-group-item" v-for="(item, index) in filterTodos" @dblclick="editTodo(item)">
-          <div class="d-flex" v-if="item.id !== cacheTodo.id">
-            <div class="form-check">
-              <input type="checkbox" class="form-check-input" :id="item.id" v-model="item.completed"/>
-              <label class="form-check-label" :class="{'completed': item.completed}" :for="item.id"
-                >{{ item.title }}
-              </label>
-            </div>
-            <button
-              type="button"
-              class="close ml-auto"
-              aria-label="Close"
-              @click="removeTodo(item)"
-            >
-              <span aria-hidden="true">&times;</span>
-            </button>
-          </div>
-          <div class="input-group mb-2">
-          <input type="text" class="form-control" v-if="item.id === cacheTodo.id"
-          v-model="cacheTitle" @keyup.esc="cancelEdit" @keyup.enter="doneEdit(item)" @blur="cancelEdit"/>
-          </div>
+    <input type="text" class="form-control" placeholder="準備要做的任務" v-model="newTodo">
+    <div class="input-group-append">
+      <button class="btn btn-primary" type="button" @click="actionAddTodo">新增</button>
+    </div>
+  </div>
+  <div class="card text-center">
+    <div class="card-header">
+      <ul class="nav nav-tabs card-header-tabs">
+        <li class="nav-item">
+          <a class="nav-link" :class="{'active':visibility == 'all'}" @click="visibility='all'" href="#">全部</a>
         </li>
-        <!-- <li class="list-group-item">
-          <input type="text" class="form-control" />
-        </li> -->
+        <li class="nav-item">
+          <a class="nav-link" :class="{'active':visbility == 'active'}" @click="visibility='active'" href="#">進行中</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" :class="{'active':visibility == 'completed'}" @click="visibility='completed'" href="#">已完成</a>
+        </li>
       </ul>
-      <div class="card-footer d-flex justify-content-between">
-        <span>還有{{undoneTodo.length}}筆任務未完成</span>
-        <a href="#/contactus" @click="removeAll">清除所有任務</a>
-      </div>
     </div>
+    <ul class="list-group list-group-flush text-left">
+      <li class="list-group-item" v-for="(item,index) in filteredTodos" @dblclick="editTodo(item)">
+        <div class="d-flex"  v-if="item.key !== cacheTodo.key">
+          <div class="form-check">
+            <input type="checkbox" class="form-check-input"  v-model="item.completed">
+            <label class="form-check-label" :class="{'completed':item.completed}">
+              {{item.title}}
+            </label>
+          </div>
+          <button type="button" class="close ml-auto" aria-label="Close" @click="deleteTodo(item.key)">
+            <span aria-hidden="true">&times;</span>
+          </button>
+        </div>
+        <input type="text" class="form-control" v-if="item.key == cacheTodo.key" 
+        v-model="cacheTodo.title" @keyup.esc="cancelEdit" @blur="cancelEdit" @keyup.enter="actionEditTodo">
+      </li>
+    </ul>
+    <div class="card-footer d-flex justify-content-between">
+      <span>還有{{undoList.length}}筆任務未完成</span>
+      <a href="#" @click="removeAll">清除所有任務</a>
+    </div>
+  </div>
   </div>
 </template>
 <script>
-let todokey = 1;
+import {mapGetters, mapActions} from 'vuex';
 export default {
-  data() {
+  data(){
     return {
+      cacheTodo:{},
       visibility: 'all',
-      newTodo: "",
-      todos: [
-        {
-          key: 0,
-          id: "123",
-          title: "代辦事項1",
-          completed: true,
-        },
-      ],
-      cacheTodo: {},
-      cacheTitle:'',
-    };
-  },
-  methods: {
-    addTodo: function () {
-      var value = this.newTodo.trim();
-      var timeStamp = Math.floor(Date.now());
-      if (!value) {
-        return;
-      }
-      this.todos.push({
-        key: todokey,
-        id: timeStamp,
-        title: value,
-        completed: false,
-      });
-      this.newTodo = "";
-      todokey++;
-    },
-    removeTodo: function (todo) {
-      var vm = this;
-      var newIndex = vm.todos.findIndex(function (item, index) {
-        return todo.id === item.id;
-      });
-      this.todos.splice(newIndex, 1);
-    },
-    editTodo: function (item) {
-      console.log(item);
-      this.cacheTodo = item;
-      this.cacheTitle = item.title;
-    },
-    cancelEdit: function(){
-      this.cacheTodo = {};
-    },
-    doneEdit: function(item){
-      item.title = this.cacheTitle;
-      this.cacheTitle = '';
-      this.cacheTodo = {};
-    },
-    removeAll: function() {
-      this.todos = [];
+      newTodo: '',
     }
   },
-  computed: {
-      filterTodos: function() {
-          if(this.visibility == 'all'){
-              return this.todos;
-          }else if(this.visibility == 'active'){
-              var newTodos = [];
-              this.todos.forEach((item)=>{
-                  if(!item.completed){
-                      newTodos.push(item);
-                  }
-              })
-              return newTodos;
-          }else if(this.visibility == 'completed'){
-              var newTodos = this.todos.filter((item)=>item.completed)
-              return newTodos;
-          }
-      },
-      undoneTodo: function() {
-        return this.todos.filter(todo => todo.completed != true);
-      }
+  methods: { ...mapActions([
+    'deleteTodo',
+    'updateTodo',
+    'removeAll',
+  ]),
+  actionAddTodo(){
+    this.$store.dispatch('addTodo',this.newTodo);
+    this.newTodo='';
+  },
+  editTodo(item){
+    this.cacheTodo = item;
+  },
+  cancelEdit(){
+    this.cacheTodo = {};
+  },
+  actionEditTodo(){
+    this.$store.dispatch('updateTodo',this.cacheTodo);
+    this.cacheTodo = {};
   }
-};
+
+  },
+  computed: { ...mapGetters({
+    todoList: 'getTodo',
+    doneList: 'getDone',
+    undoList: 'getUndo',
+  }),
+    filteredTodos() {
+    if(this.visibility == 'all'){
+      return this.todoList;
+    }else if(this.visibility == 'active'){
+      return this.undoList;
+    }else if(this.visibility == 'completed'){
+      return this.doneList;
+    }
+  }
+  }
+}
 </script>
 <style>
 .completed {
---
+++ src/store/module/todo.js
@@ -0,0 +1,90 @@
+const types = {
+    CREATE_TODO: 'todo/CREATE_TODO',
+    REMOVEALL_TODO: 'todo/REMOVEALL_TODO',
+    DELETE_TODO: 'todo/DELETE_TODO',
+    UPDATE_TODO: 'todo/UPDATE_TODO',
+  }
+
+  const state = {
+    todos: [
+        {
+            key: 0,
+            title: "代辦事項1",
+            completed: true,
+          },
+    ]
+  }
+
+  const getters = {
+      getTodo (state) {
+          return state.todos;
+      },
+      getDone (state) {
+          return state.todos.filter( item => item.completed === true);
+      },
+      getUndo (state) {
+          return state.todos.filter( item => !item.completed);
+      }
+  }
+
+  const actions = {
+    addTodo ({ commit }, newTodo) {
+      commit(types.CREATE_TODO, newTodo);
+    },
+    removeAll ({ commit }) {
+      commit(types.REMOVEALL_TODO);
+    },
+    deleteTodo ({ commit }, key) {
+      commit(types.DELETE_TODO, key);
+    },
+    updateTodo ({ commit }, obj) {
+      commit(types.UPDATE_TODO, obj);
+    },
+  }
+
+  // 流水 key
+let todoKey = state.todos.length;
+
+const mutations = {
+    //新增
+    [types.CREATE_TODO](state, newTodo){
+        var value = newTodo.trim();
+        if(!value){
+            return
+        }
+        state.todos.push({
+            key: todoKey,
+            title: newTodo,
+            completed: false,
+        });
+        todoKey++;
+    },
+    //刪除
+    [types.DELETE_TODO](state,key){
+        state.todos.forEach((item,index) => {
+            if(item.key === key){
+                state.todos.splice(index,1);
+            }
+        });
+    },
+    //更新
+    [types.UPDATE_TODO](state,obj){
+        state.todos.forEach((item,index) =>{
+            if(item.key === obj.key){
+                item.title = obj.title;
+            }
+        });
+    },
+    //刪除全部
+    [types.REMOVEALL_TODO](state){
+        state.todos = [];
+    }
+
+}
+
+export default {
+    state,
+    getters,
+    actions,
+    mutations
+  }
